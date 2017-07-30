3899cdc9b457c97ce85c5faead98a4405a578144
Tigris <Tigris_wzd@163.com>
2017-07-30 10:34:25 +0800

    关于外卖的Webapp

---
+++ src/components/header/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header">
+    I am header!
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Goods from '../components/goods/Goods.vue'
+import Seller from '../components/seller/Seller.vue'
+import Ratings from '../components/ratings/Ratings.vue'
+
+Vue.use(Router)
+const routes = [
+  {
+      path: '/goods',
+      name: 'goods',
+      component: Goods
+    },
+    {
+      path: '/ratings',
+      name: 'ratings',
+      component: Ratings
+    },
+    {
+      path: '/seller',
+      name: 'seller',
+      component: Seller
+    }
+]
+
+export default new Router({
+  linkActiveClass: 'router-link-active',
+  routes
+})
