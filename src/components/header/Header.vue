548eb48b1f86e4128608c0c7763cef01490eb007
Tigris <Tigris_wzd@163.com>
2017-08-01 18:49:11 +0800

    完成浮层(icon字体图标不显示)

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -29,11 +29,46 @@
     <div class="background">
       <img :src="seller.avatar">
     </div>
-    <div class="detail" v-show="isShow"></div>
+    <div class="detail" v-show="isShow">
+      <div class="detail-wrapper clearfix">
+      	<div class="detail-main">
+      	  <h1 class="name">{{ seller.name }}</h1>
+      	  <div class="star-wrapper">
+      	  	<star :score="seller.score" :size="48"></star>
+      	  </div>
+      	  <div class="title">
+      	  	<div class="line"></div>
+      	  	<div class="text">优惠信息</div>
+      	  	<div class="line"></div>
+      	  </div>
+      	  <div class="supportsAll">
+      	  	<ul>
+      	  	  <li v-for="support in seller.supports">
+      	  	  	<span class="simg" v-bind:class="classMap[support.type]"></span>
+      	  	  	<span class="stext">{{ support.description }}</span>
+      	  	  </li>
+      	  	</ul>
+      	  </div>
+      	  <div class="title">
+      	  	<div class="line"></div>
+      	  	<div class="text">商家公告</div>
+      	  	<div class="line"></div>
+      	  </div>
+      	  <div class="detail-bulletin">
+      	  	<span class="dbtext">{{ seller.bulletin }}</span>
+      	  </div>
+      	</div>
+      </div>
+      <div class="detail-close">
+      	<i class="icon-close"></i>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import star from '../star/star'
+
   export default {
     props: {
       seller: {
@@ -52,6 +87,9 @@
     },
     created() {
       this.classMap = ['decrease', 'discount', 'special', 'guarantee', 'invoice']
+    },
+    components: {
+      star
     }
   }
 </script>
@@ -189,5 +227,83 @@
       left: 0
       overflow: auto
       background-color: rgba(7, 17, 27, 0.8)
-      // filter: blur(10px)  
+      // filter: blur(10px)
+      .detail-wrapper
+      	min-height: 100%
+      	width: 100%
+      	.detail-main
+      	  margin-top: 64px
+      	  padding-bottom: 64px
+      	  font-size: 12px
+      	  color: rgb(255, 255, 255)
+      	  .name
+      	  	line-height: 16px
+      	  	text-align: center
+      	  	font-size: 16px
+      	  	font-weight: 700 
+      	  .star-wrapper
+      	  	margin-top: 18px
+      	  	padding: 2px 0
+      	  	text-align: center
+      	  .title
+      	  	display: flex
+      	    // width: 80%
+      	    margin: 28px auto 24px auto
+      	    padding: 0 31px
+      	    .line
+      	      flex: 1
+      	      position: relative
+      	      top: -6px
+      	      border-bottom: 1px solid rgba(255, 255, 255, 0.2)
+      	    .text
+      	      padding: 0 12px
+      	      font-size: 14px
+      	  .supportsAll
+      	  	padding: 0 31px
+      	  	margin: 24px auto 28px auto
+      	  	ul
+      	      padding: 0 12px
+      	      li
+      	      	margin-bottom: 12px
+      	      	font-size: 0
+      	      	.simg
+      	      	  display: inline-block
+      	      	  margin-right: 6px
+      	      	  width: 16px
+      	      	  height: 16px
+      	      	  background-size: 16px 16px
+      	      	  vertical-align: top
+      	      	  &.decrease
+          	        bg-image('decrease_1')
+          	      &.discount
+          	        bg-image('discount_1')
+          	      &.guarantee
+          	        bg-image('guarantee_1')
+          	      &.invoice
+          	        bg-image('invoice_1')
+          	      &.special
+          	        bg-image('special_1')
+      	      	.stext
+      	      	  vertical-align: top
+      	      	  font-size: 12px
+      	      	  font-weight: 200
+      	      	  color: rgb(255, 255, 255)
+      	      	  line-height: 12px
+      	      li.last-child
+      	      	margin-bottom: 0
+      	  .detail-bulletin
+      	  	padding: 0 31px
+      	  	.dbtext
+      	  	  padding: 0 12px
+      	  	  font-size: 12px
+      	  	  font-weight: 200
+      	  	  color: rgb(255, 255, 255)
+      	  	  line-height: 24px
+      .detail-close
+      	position: relative
+      	width: 32px
+      	height: 32px
+      	margin: -64px auto 0 auto
+      	clear: both
+      	font-size: 32px
 </style>
